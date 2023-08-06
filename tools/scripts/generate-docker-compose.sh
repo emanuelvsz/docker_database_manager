a7cff9034acc46c41a8492c5eaa9c07395728fc9
emanuelvsz <usermane13@gmail.com>
2023-08-05 21:08:48 -0300

    creating function that create a Dockerfile

--- tools/scripts/generate-docker-compose.sh
+++ tools/scripts/generate-docker-compose.sh
@@ -1,37 +1,49 @@
 #!/bin/bash
 
-function generate_compose_file() {
+function generate_dockerfile() {
     local database_type=$1
+
+    local dockerfile_path="./tools/Dockerfile.$database_type"
+
     if [ "$database_type" == "mongo" ]; then
-        cat > docker-compose.yaml <<EOL
-version: '3'
-services:
-  mongo:
-    container_name: default_container
-    build:
-      context: .
-      dockerfile: tools/Dockerfile.mongo
-    ports:
-      - "27017:27017"
+        cat > "$dockerfile_path" <<EOL
+FROM mongo:latest
+
+ENV MONGO_PORT 27017
+
+EXPOSE \$MONGO_PORT
 EOL
     elif [ "$database_type" == "postgres" ]; then
-        cat > docker-compose.yaml <<EOL
-version: '3'
-services:
-  database:
-    container_name: default_container
-    build:
-      context: .
-      dockerfile: tools/Dockerfile.postgres
-    env_file:
-      - ./tools/.env
-    ports:
-      - "5432:5432"
+        cat > "$dockerfile_path" <<EOL
+FROM postgres:14
+
+COPY tools/database/fixtures /database/fixtures
+COPY tools/database /tmp/database
+RUN find /tmp/database -type f -exec cp {} /docker-entrypoint-initdb.d/ \;
+
+RUN rm -r /tmp/database
+
+EXPOSE 5432
 EOL
     else
         echo "Invalid option. Use 'mongo' or 'postgres'."
         return 1
     fi
+}
+
+function generate_compose_file() {
+    local database_type=$1
+    generate_dockerfile "$database_type"
+
+    cat > docker-compose.yaml <<EOL
+version: '3'
+services:
+  $database_type:
+    container_name: default_container
+    build:
+      context: .
+      dockerfile: ./tools/Dockerfile.$database_type
+EOL
 }
 
 if [ -z "$1" ]; then
